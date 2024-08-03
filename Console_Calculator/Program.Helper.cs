85b82a9ad945c8b7b530abfa04cef4328551b02e
MirzaeiMohadeseh <mirzaeimohadeseh2003@gmail.com>
2024-08-03 10:08:52 +0330

    1st commit

---
+++ Console_Calculator/Program.Helper.cs
@@ -0,0 +1,18 @@
+﻿using static System.Console;
+partial class Program
+{
+    private static void SectionTitile(string title)
+    {
+        ConsoleColor previousColor = ForegroundColor;
+        ForegroundColor = ConsoleColor.DarkCyan;
+        WriteLine($"*** {title} ***");
+        ForegroundColor = previousColor;
+    }
+    private static void ResultTitle(string title)
+    {
+        ConsoleColor previousColor = ForegroundColor;
+        ForegroundColor = ConsoleColor.Green;
+        WriteLine($"{title}");
+        ForegroundColor = previousColor;
+    }
+}
---
+++ Console_Calculator/Program.cs
@@ -0,0 +1,37 @@
+﻿using static System.Console;
+WriteLine("------------------------------");
+SectionTitile("Console Calculator");
+WriteLine("------------------------------");
+WriteLine("Ready to use!");
+do
+{
+double FNum = 0;
+double SNum = 0;
+Write("Enter the first number: ");
+FNum= Convert.ToDouble(Console.ReadLine());
+
+Write("Enter the second number: ");
+SNum = Convert.ToDouble(Console.ReadLine());
+
+Write("Choose an operation -> (+  -  * /): ");
+string? Oper = ReadLine();
+
+    switch (Oper)
+    {
+        case "+":
+            ResultTitle($"The result is: {FNum} + {SNum} = {FNum + SNum}");
+            break;
+        case "-":
+            ResultTitle($"The result is: {FNum} - {SNum} = {FNum - SNum}");
+            break;
+        case "*":
+            ResultTitle($"The result is: {FNum} * {SNum} = {FNum * SNum}");
+            break;
+        case "/":
+            ResultTitle($"The result is: {FNum} / {SNum} = {FNum / SNum}");
+            break;
+
+    }
+    WriteLine();
+    Write("Do you want to Continue? Y/N: ");
+} while (ReadLine()?.ToUpper() == "Y");
